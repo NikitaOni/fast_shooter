89d8a98283a6e159c98355d346ee518873abbce1
NikitaOni <nikita.kovrigin.k@gmail.com>
2024-01-05 18:46:34 +0300

    Add spawner levels and light bake, fix ground minimap

---
+++ Assets/Scripts/LevelCount.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCount : MonoBehaviour
+{
+    public static int levelCount =  5;
+
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            int count = i + 1;
+            if (count < levelCount)
+            {
+                transform.GetChild(i).GetComponent<Image>().color = new Color32(231, 202, 16, 255);
+            }
+            else if(count == levelCount)
+            {
+                transform.GetChild(i).GetComponent<Image>().color = new Color32(0, 210, 30, 255);
+            }
+            else
+            {
+                transform.GetChild(i).GetComponent<Image>().color = new Color32(115, 115, 115, 255);
+            }
+        }
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
@@ -9,6 +10,55 @@ public class LevelManager : MonoBehaviour
     public static int enemyCount;
 
     [SerializeField] private Text counter;
+
+    [Header("Level Number")]
+    [SerializeField] GameObject level_1;
+    [SerializeField] GameObject level_2;
+    [SerializeField] GameObject level_3;
+    [SerializeField] GameObject level_4;
+    [SerializeField] GameObject level_5;
+    [SerializeField] GameObject level_6;
+    [SerializeField] GameObject level_7;
+    [SerializeField] GameObject level_8;
+    [SerializeField] GameObject level_9;
+    [SerializeField] GameObject level_10;
+
+    private void Start()
+    {
+        switch (LevelCount.levelCount)
+        {
+            case 1:
+                Instantiate(level_1);
+                break;
+            case 2:
+                Instantiate(level_2);
+                break;
+            case 3:
+                Instantiate(level_3);
+                break;
+            case 4:
+                Instantiate(level_4);
+                break;
+            case 5:
+                Instantiate(level_5);
+                break;
+            case 6:
+                Instantiate(level_6);
+                break;
+            case 7:
+                Instantiate(level_7);
+                break;
+            case 8:
+                Instantiate(level_8);
+                break;
+            case 9:
+                Instantiate(level_9);
+                break;
+            case 10:
+                Instantiate(level_10);
+                break;
+        }
+    }
 
     private void Update()
     {
