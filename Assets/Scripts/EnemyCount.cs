b10b569c0b861a508930a5ef2264fc7e7c11d0ec
NikitaOni <nikita.kovrigin.k@gmail.com>
2024-01-06 12:54:05 +0300

    Add reload levels afther finish 10 levels

--- Assets/Scripts/EnemyCount.cs
+++ Assets/Scripts/EnemyCount.cs
@@ -16,8 +16,16 @@ public class EnemyCount : MonoBehaviour
         LevelManager.enemyCount--;
         if(LevelManager.enemyCount == 0)
         {
-            LevelCount.levelCount++;
-            SceneManager.LoadScene(0);
+            if(LevelCount.levelCount < 10)
+            {
+                LevelCount.levelCount++;
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                LevelCount.levelCount = 1;
+                SceneManager.LoadScene(0);
+            }
         }
     }
 }
