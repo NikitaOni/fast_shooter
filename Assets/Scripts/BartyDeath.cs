fe79b4d144cc1c2ca77dc96e28d85a43d88bc276
NikitaOni <nikita.kovrigin.k@gmail.com>
2024-01-06 12:24:28 +0300

    Add deley before Barty death

--- Assets/Scripts/BartyDeath.cs
+++ Assets/Scripts/BartyDeath.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class BartyDeath : MonoBehaviour
 {
+
+    private IEnumerator DeleyDeath()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(0);
+    }
     public void Death()
     {
-        SceneManager.LoadScene(0);
+        StartCoroutine(DeleyDeath());
     }
 }
