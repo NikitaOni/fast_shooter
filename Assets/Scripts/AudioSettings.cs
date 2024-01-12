50a285bd1bc87665fa9d63dd2ae50f60038ac30f
NikitaOni <nikita.kovrigin.k@gmail.com>
2024-01-12 17:47:55 +0300

    Add slider for music and sound effects

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class AudioSettings : MonoBehaviour
 {
+    public string volumeParameter = "MasterVolume";
+    public AudioMixer Mixer;
+    public Slider slider;
 
-    public AudioMixerGroup Mixer;
-    private void Start()
+    private float _volumeValue;
+    private const float _multipyer = 20f;
+
+    private void Awake()
     {
-        
+        slider.onValueChanged.AddListener(HandleSliderValueChanged);
     }
 
-    public void ChangeVolume(float volume)
+    public void HandleSliderValueChanged(float value)
     {
-        Mixer.audioMixer.SetFloat("Music", Mathf.Lerp(-80,0,volume));
-    } 
+        _volumeValue = Mathf.Log10(value)* _multipyer;
+        Mixer.SetFloat(volumeParameter, _volumeValue);
+    }
+
+    private void Start()
+    {
+
+        _volumeValue = PlayerPrefs.GetFloat(volumeParameter, Mathf.Log10(slider.value) * _multipyer);
+
+        Mixer.SetFloat(volumeParameter, _volumeValue);
+        slider.value = Mathf.Pow(10f, _volumeValue / _multipyer);
+   
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(volumeParameter, _volumeValue);
+    }
 }
