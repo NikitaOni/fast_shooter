b495c54beedd59e858a93615c88b3f899940d2c3
NikitaOni <nikita.kovrigin.k@gmail.com>
2024-01-19 15:56:17 +0300

    Optimization

---
+++ Assets/Scripts/Yandex/Yandex.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Yandex : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void RateGame();
+
+
+
+    public void RateGameButton()
+    {
+        RateGame();
+    }
+}
