6f31a02c5bad9bb4b7a71c1bc7941b3ac24bb025
NikitaOni <nikita.kovrigin.k@gmail.com>
2024-01-30 23:35:05 +0300

    Add icon boss and fix canvas ui hp bar

--- Assets/Scripts/LevelCount.cs
+++ Assets/Scripts/LevelCount.cs
@@ -11,7 +11,7 @@ public class LevelCount : MonoBehaviour
     {
         if(PlayerPrefs.HasKey("Level") == false)
         {
-            levelCount = 10;
+            levelCount = 1;
         }
         else
         {
